9b2b43efdb7a1b3ef264e05f874ea7ef23b1c4ff
mihai.buzgau <mihai.buzgau@uhurusoftware.com>
2013-03-18 07:10:18 -0700

    added nagios to bosh release
    Change-Id: I39230568df78f8147673501113b0a882884e5efe

---
+++ release/jobs/nagios_dashboard/templates/config.inc.php
@@ -0,0 +1,20 @@
+<?php
+//
+
+
+$cfg['cgi_config_file']='/var/vcap/packages/nagios/etc/cgi.cfg';  // location of the CGI config file
+
+$cfg['cgi_base_url']='/nagios/cgi-bin';
+
+
+// FILE LOCATION DEFAULTS
+$cfg['main_config_file']='/var/vcap/jobs/nagios_dashboard/conf/nagios.cfg';  // default location of the main Nagios config file
+$cfg['status_file']='/var/vcap/packages/nagios/var/status.dat'; // default location of Nagios status file
+$cfg['state_retention_file']='/var/vcap/store/nagios/retention.dat'; // default location of Nagios retention file
+
+
+
+// utilities
+require_once(dirname(__FILE__).'/includes/utils.inc.php');
+
+?>
