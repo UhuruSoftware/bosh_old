7be4b8de1b16f8414680065292c367be68d93e94
florindragos <florind@uhurusoftware.com>
2013-03-21 12:57:17 +0200

    Fixed nagios job to work with the commander
    Change-Id: I340d7525289515591ec8b401dcc408433bf5c879

--- release/jobs/nagios_dashboard/templates/config.inc.php
+++ release/jobs/nagios_dashboard/templates/config.inc.php
@@ -8,7 +8,7 @@ $cfg['cgi_base_url']='/nagios/cgi-bin';
 
 
 // FILE LOCATION DEFAULTS
-$cfg['main_config_file']='/var/vcap/jobs/nagios_dashboard/conf/nagios.cfg';  // default location of the main Nagios config file
+$cfg['main_config_file']='/var/vcap/jobs/nagios_dashboard/config/nagios.cfg';  // default location of the main Nagios config file
 $cfg['status_file']='/var/vcap/packages/nagios/var/status.dat'; // default location of Nagios status file
 $cfg['state_retention_file']='/var/vcap/store/nagios/retention.dat'; // default location of Nagios retention file
 
--- release/jobs/nagios_dashboard/templates/nagios_reload.sh
+++ release/jobs/nagios_dashboard/templates/nagios_reload.sh
@@ -6,15 +6,9 @@ export LIBRARY_PATH=/var/vcap/packages/libpq/lib:$LIBRARY_PATH
 export PATH=/var/vcap/packages/ruby/bin:$PATH
 LOG_DIR=/var/vcap/sys/log/nagios
 
-ruby /var/vcap/packages/nagios_dashboard/nagios_dashboard/bin/generate_nagios_config.rb -s /var/vcap/store/nagios/etc/serviceext >>$LOG_DIR/generate_nagios_config.stdout.log 2>>$LOG_DIR/generate_nagios_config.stderr.log
+ruby /var/vcap/packages/nagios_dashboard/nagios_dashboard/bin/generate_nagios_config.rb >>$LOG_DIR/generate_nagios_config.stdout.log 2>>$LOG_DIR/generate_nagios_config.stderr.log
 
 if [ `md5sum /var/vcap/packages/nagios_dashboard/nagios_dashboard/config/uhuru-hosts.cfg | awk '{print $1}'` != `md5sum /var/vcap/packages/nagios_dashboard/nagios_dashboard/config/uhuru-hosts.cfg.old | awk '{print $1}'` ] ; then
   chmod 777 /var/vcap/packages/nagios_dashboard/nagios_dashboard/config/uhuru-hosts.cfg
   /var/vcap/jobs/nagios_dashboard/bin/nagios reload
 fi
-
-if [ `md5sum /var/vcap/packages/nagios_dashboard/nagios_dashboard/config/uhuru-ngraph.ncfg | awk '{print $1}'` != `md5sum /var/vcap/packages/nagios_dashboard/nagios_dashboard/config/uhuru-ngraph.ncfg.old | awk '{print $1}'` ] ; then
-  chmod 777 /var/vcap/packages/nagios_dashboard/nagios_dashboard/config/uhuru-ngraph.ncfg.cfg
-  /var/vcap/jobs/nagios_dashboard/bin/nagios_grapher restart
-fi
-
