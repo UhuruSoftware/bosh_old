1317992f4d34f1bac7553df90cd167e0a6cae1ec
vlad.iovanov <vlad.iovanov@uhurusoftware.com>
2013-04-03 14:28:37 +0300

    Bugfixing to nagios release.
    Change-Id: Icac290527175f818fec71cf23f90e7462521a5fe

--- release/jobs/nagios_dashboard/templates/nagios_reload.sh
+++ release/jobs/nagios_dashboard/templates/nagios_reload.sh
@@ -6,7 +6,10 @@ export LIBRARY_PATH=/var/vcap/packages/libpq/lib:$LIBRARY_PATH
 export PATH=/var/vcap/packages/ruby/bin:$PATH
 LOG_DIR=/var/vcap/sys/log/nagios
 
-ruby /var/vcap/packages/nagios_dashboard/nagios_dashboard/bin/generate_nagios_config.rb >>$LOG_DIR/generate_nagios_config.stdout.log 2>>$LOG_DIR/generate_nagios_config.stderr.log
+export GEM_HOME=/var/vcap/packages/nagios_dashboard/gem_home
+export BUNDLE_GEMFILE=/var/vcap/packages/nagios_dashboard/nagios_dashboard/Gemfile
+
+bundle exec ruby /var/vcap/packages/nagios_dashboard/nagios_dashboard/bin/generate_nagios_config.rb >>$LOG_DIR/generate_nagios_config.stdout.log 2>>$LOG_DIR/generate_nagios_config.stderr.log
 
 if [ `md5sum /var/vcap/packages/nagios_dashboard/nagios_dashboard/config/uhuru-hosts.cfg | awk '{print $1}'` != `md5sum /var/vcap/packages/nagios_dashboard/nagios_dashboard/config/uhuru-hosts.cfg.old | awk '{print $1}'` ] ; then
   chmod 777 /var/vcap/packages/nagios_dashboard/nagios_dashboard/config/uhuru-hosts.cfg
