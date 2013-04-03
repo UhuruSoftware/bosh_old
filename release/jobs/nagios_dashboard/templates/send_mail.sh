0bb11935641ba461a84587ef3bcdbfe87d98ef18
florindragos <florind@uhurusoftware.com>
2013-04-03 15:10:27 +0300

    Nagios can now listen only to localhost and can be used without authorization
    Change-Id: I07a5604ed9d371c3fd6b0b2f31309c42db8a06ee

--- release/jobs/nagios_dashboard/templates/send_mail.sh
+++ release/jobs/nagios_dashboard/templates/send_mail.sh
@@ -1,4 +1,4 @@
 export C_INCLUDE_PATH=/var/vcap/packages/libpq/include:$C_INCLUDE_PATH
 export LIBRARY_PATH=/var/vcap/packages/libpq/lib:$LIBRARY_PATH
 export PATH=/var/vcap/packages/ruby/bin:$PATH
-ruby /var/vcap/packages/nagios_dashboard/nagios_dashboard/bin/send_mail.rb "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
+ruby /var/vcap/packages/nagios_dashboard/nagios_dashboard/bin/send_mail.rb "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8"
